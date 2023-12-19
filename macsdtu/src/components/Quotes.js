4ffa68aee01289875a5e8ee9d22c53f6249af1e4
Aditya Mishra <adityavishu03@gmail.com>
2023-12-19 17:44:39 +0530

    home component updated

--- macsdtu/src/components/Quotes.js
+++ macsdtu/src/components/Quotes.js
@@ -4,7 +4,7 @@ const Quotes = (props) => {
   return (
     <div>
         {props.msg.map((ele,index)=>{
-            return <p key={index} className={`text-${ele.color} text-${ele.size}`}>{ele.message}</p>
+            return <p key={index} className={`${ele.color} ${ele.size}`}>{ele.message}</p>
         })
         }
     </div>
