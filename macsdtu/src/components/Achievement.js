4e3f43f0ad5f954ac8026db7a7b555175fd6f85c
Aman Gupta <techbugaman@gmail.com>
2023-12-21 00:51:17 +0530

    Achievement Component Completed

--- macsdtu/src/components/Achievement.js
+++ macsdtu/src/components/Achievement.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import Quotes from './Quotes'
+import AchievementBox from './Achievementbox'
+import Dummy from '../images/dummy.jpg'
+let achievement_quote = [{ message: "In the tapestry of success, our members weave remarkable threads.", color: "text-white", size: "text-3xl" }, { message: "Celebrating achievements, one milestone at a time,", color: "text-neutral-500", size: "text-3xl" }, { message: "as our society members shine brightly on their unique paths.", color: "text-neutral-500", size: "text-3xl" }]
 
 export default function Achievement() {
   return (
-    <div className='mt-3 flex justify-center items-center text-3xl font-bold'>TBD</div>
+    <div className="event-section flex flex-col justify-center mt-16">
+      {/* Quote */}
+      <div className="qoutes text-center flex flex-col items-center mx-10 mb-4">
+        <Quotes msg={achievement_quote} />
+      </div>
+
+      {/* Achievements Wall */}
+      <div className="achievement-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-3 mx-14 mt-10">
+        <AchievementBox img={Dummy} msg={`"Our President Mr. Nissan Kumar is selected for ICPC Regionals Amarpali"`} />
+        <AchievementBox img={Dummy} msg={`"Our ML Head Mr. Divyanshu Bansal is selected for SIH Grand Finale in Hyderabad"`} />
+        <AchievementBox img={Dummy} msg={`"Our Tech Head Mr. Ayush Gupta secured a perfect 10 SGPA in his 1st semester"`} />
+        <AchievementBox img={Dummy} msg={`"Our ex-VP Mr. Ishan Bhateja is currently serving as a Placement Coordinator"`} />
+        <AchievementBox img={Dummy} msg={`"Kuch bhi"`} />
+      </div>
+    </div>
   )
 }
---
+++ macsdtu/src/components/Achievementbox.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import AchievementBorder from '../images/achievementBorder.svg'
+
+export default function AchievementBox(props) {
+    return (
+        <div className="achievement-box">
+            <figure className="rounded-xl relative p-8">
+                <img className="w-24 h-24 rounded-full mx-auto" src={props.img} alt="" />
+                <img className="w-60 h-60 aspect-square absolute -inset-8 mx-auto bg-transparent" src={AchievementBorder} alt="" />
+                <div className="bg-transparent pt-6 text-center space-y-4 mt-5">
+                    <blockquote className='bg-transparent'>
+                        <p className="text-lg font-medium bg-transparent">
+                            {props.msg}
+                        </p>
+                    </blockquote>
+                </div>
+            </figure>
+        </div>
+    )
+}
