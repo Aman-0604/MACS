f7d6a936b3e64575b273b581b024691fa2109ba1
Aman Gupta <techbugaman@gmail.com>
2023-12-21 01:42:27 +0530

    Sponsors component done

--- macsdtu/src/components/Sponsor.js
+++ macsdtu/src/components/Sponsor.js
@@ -1,28 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Quotes from './Quotes'
-import ImageBox from './Imagebox'
-import Dummy from '../images/dummy.jpg'
+import SponsorBox from './Sponsorbox'
+import Sponsor1 from '../images/Sponsor1.png'
+import Sponsor2 from '../images/Sponsor2.png'
+import Sponsor3 from '../images/Sponsor3.png'
+import Sponsor4 from '../images/Sponsor4.png'
 let sponsor_quote = [{ message: "Gratitude in partnership, impact in support.", color: "text-white", size: "text-3xl" }, { message: "To our sponsors, thank you for being the cornerstone of our success.", color: "text-neutral-500", size: "text-3xl" }]
 
 export default function Sponsor() {
   return (
     <div className="event-section flex flex-col justify-center mt-16">
       {/* Quote */}
-      <div className="qoutes text-center flex flex-col items-center mb-4">
+      <div className="qoutes text-center flex flex-col items-center mb-4 mx-10">
         <Quotes msg={sponsor_quote} />
       </div>
 
       {/* Sponsor */}
-      <div className="sponsor-section flex flex-col justify-center items-center mt-10">
-        <div className="sponsor-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-44 gap-y-20 mt-16">
-          <ImageBox img={Dummy} />
-          <ImageBox img={Dummy} />
-          <ImageBox img={Dummy} />
-          <ImageBox img={Dummy} />
-          <ImageBox img={Dummy} />
-          <ImageBox img={Dummy} />
-        </div>
+      <div className="sponsor-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-x-1 gap-y-10 mx-14 mt-10">
+        <SponsorBox img={Sponsor1} />
+        <SponsorBox img={Sponsor2} />
+        <SponsorBox img={Sponsor3} />
+        <SponsorBox img={Sponsor4} />
       </div>
 
       {/* Sponsor Us Button */}
---
+++ macsdtu/src/components/Sponsorbox.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Spnosorbox(props) {
+    return (
+        <div className="sponsor-box">
+            <img className="w-40 h-40 aspect-square mx-auto block object-scale-down" src={props.img} alt="" />
+        </div>
+    )
+}
