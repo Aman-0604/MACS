354de653a7c96de0bbcd4253a39d4dfa2b3cff0e
Aman Gupta <techbugaman@gmail.com>
2024-01-04 12:34:24 +0530

    Added two feedbacks

--- macsdtu/src/components/Messagebox.js
+++ macsdtu/src/components/Messagebox.js
@@ -7,7 +7,7 @@ export default function Messagebox(props) {
                 <img className="w-24 h-24 rounded-full mx-auto block object-cover object-center" src={props.img} alt="" />
                 <div className="bg-transparent pt-6 text-center space-y-4">
                     <blockquote className='bg-transparent'>
-                        <p className="text-sm md:text-lg font-medium bg-transparent">
+                        <p className="text-sm md:text-lg font-medium bg-transparent text-justify">
                             {props.message}
                         </p>
                     </blockquote>
